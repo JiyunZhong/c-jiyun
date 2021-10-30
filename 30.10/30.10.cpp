112cea37203cefe70c1c9faeba419a3148ea39f7
JiyunZhong <jiyunzhong1024@gmail.com>
2021-10-31 01:30:52 +0800

    1

--- 30.10/30.10.cpp
+++ 30.10/30.10.cpp
@@ -1,7 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//int main()
+//{
+//	printf("halloworld\n");
+//	return 0;
+//}
+//typedef unsigned int u_int;
+//int main()
+//{
+//	unsigned int num = 100;
+//	u_int num2 = 10;
+//	return 0;
+//}
+//void test()
+//{
+//	int a = 1;
+//	a++;
+//	printf("%d",a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//
+//	}
+//	return 0;
+//}
+//#define MAX 1000
+//int main()
+//{
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+#define Add(X,Y)(X+Y)
 int main()
 {
-	printf("halloworld\n");
+	printf("%d\n", 4*Add(2, 3));
 	return 0;
 }
