5b572c2c0e9b01e6b9a6b8f1fd9db5afc37260e5
JiyunZhong <jiyunzhong1024@gmail.com>
2021-11-04 22:41:46 +0100

    4.11

---
+++ 3.11/p28.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int Max(int x,int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b); 
+//	int m= Max(a, b);
+//	printf("%d\n", m);
+//	return 0;
+//}
+int main()
+{
+	const int num = 0;
+	//int num = 20;
+	printf("%d\,", num);
+	return 0;
+}
---
+++ 4.11/p28p30.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int m;
+//	scanf("%d", &m);
+//	if (m % 5 == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int age =0;
+//	scanf("%d", &age);
+//	if (age >= 18)
+//		printf("成年\n");
+//	else if (age < 16,age>10)
+//		printf("青年\n");
+//	else
+//		printf("孩子\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (1 == i % 2)
+//			printf("%d\n", i);
+//	}
+//	return 0;
+//}
